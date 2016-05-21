fd5d45c2a17308148cb6a12472394fb88a1b8d08
Aidan Sawyer <aks5238@rit.edu>
2016-05-21 19:47:35 +0000

    add sample text to aid in visualizing display

--- index.php
+++ index.php
@@ -58,22 +58,21 @@
     
     <div class="container">
       <div class="wrapper">
-        
         <div class="row">
           
-          <div class="col-xs-2 left" id="navigationPane">
+          <!-- left pane with filters to control display on right -->
+          <div class="col-xs-3 left" id="navigationPane">
             <?php include_once 'selector-pane.html'; ?>
           </div> <!-- /col-md-2 navigation -->
           
-          
+          <!-- content pane containing the actual entries display -->
           <div class="col-xs-8 col-md-10 content" id="content">	
             
-            
+            <h3></h3>
             
             <p>Soon to be list of feeds: </p>	
             
-            <p>
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
               totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae 
               dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia cor magni dolores 
               eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, 
@@ -88,7 +87,7 @@
                   <div class="panel-heading">
                     <h4 class="panel-title">
                       <a href="#<? php $entry.link ?>">
-                        <b><?php $entry.title ?></b>
+                        <b>Title of the Entry Itself<?php $entry.title ?></b>
                       </a>
                     </h4>
                   </div>
@@ -96,16 +95,19 @@
                     <div class="panel-body">
       
                       <!-- title of parent feed (e.g. Lost in the Stacks) -->
-                      <p> <b> <? php $entry.feed ?> </b> </p>
+                      <p> <b> The Awesome Times Blog <? php $entry.feed ?> </b> </p>
       
                       <!-- date published -->                
-                      <p> <u><? php $entry.publishedDate ?></u> </p>
+                      <p> <u>Thu, 08 Oct 2015 17:06:49 -0700<? php $entry.publishedDate ?></u> </p>
                       
                       <!-- categories/tags -->
-                      <p> <? php $entry.tags ?> </p>
+                      <p> Podcast, Philosophy <? php $entry.tags ?> </p>
       
                       <!-- contentSnippet -->
-                      <p> <? php $entry.contentSnippet ?> </p>
+                      <p>"Fresh brewed espresso goes into a barrel aged stout and tart raspberry contrasts caramel and chocolate malt in a barrel aged ..."</p>
+                      <p> This is an example of a content snippet from a given entry that is text. It shows the first N words followed by an elipse. ...
+                        <? php $entry.contentSnippet ?> 
+                      </p>
                       
                     </div>
                   </div>
